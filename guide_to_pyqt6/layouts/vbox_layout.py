d0d91584290bedd38e3be5326e4b63c3c06b6e2c
Hundredvisionsguy <winikkc@hsd.k12.or.us>
2024-05-01 11:32:41 -0700

    simple vbox_layout part 1 - solution

--- guide_to_pyqt6/layouts/vbox_layout.py
+++ guide_to_pyqt6/layouts/vbox_layout.py
@@ -4,7 +4,7 @@ by HundredVisionsGuy
 A simple VBox Layout - with some standard widgets
 """
 import sys
-from PyQt6.QtGui import QFont, QFontDatabase
+from PyQt6.QtGui import QFont
 from PyQt6.QtWidgets import (
     QApplication,
     QLabel,
@@ -24,11 +24,26 @@ class MainWindow(QMainWindow):
         self.setContentsMargins(12, 12, 12, 12)
         self.resize(320, 240)
 
+        # Create main layout
         layout = QVBoxLayout()
+
+        # Title label
         title_label = QLabel("Simple VBox Layout!")
+        title_label.setFont(QFont("Calibri", 22, 800, True))
+        tagline_label = QLabel("Part 1: Getting started!")
+        tagline_label.setFont(QFont("Calibri", 18, 400, True))
+
+        # Name input widget
+        name_input = QLineEdit()
+        name_input.setPlaceholderText("Your Name:")
 
         # add widgets & layouts to main layout
         layout.addWidget(title_label)
+        layout.addWidget(tagline_label)
+        layout.addWidget(name_input)
+
+        # Push contents to the top
+        layout.addStretch()
 
         widget = QWidget()
         widget.setLayout(layout)
@@ -42,4 +57,4 @@ if __name__ == "__main__":
     window = MainWindow()
     window.show()
 
-app.exec()
+    app.exec()
