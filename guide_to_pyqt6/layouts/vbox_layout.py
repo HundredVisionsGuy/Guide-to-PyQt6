9323e0a318d70a6718cedbff746f389b2ddc923f
Hundredvisionsguy <winikkc@hsd.k12.or.us>
2024-05-01 10:50:00 -0700

    adding starter code and structure

---
+++ guide_to_pyqt6/layouts/vbox_layout.py
@@ -0,0 +1,45 @@
+"""
+vbox_layout.py
+by HundredVisionsGuy
+A simple VBox Layout - with some standard widgets
+"""
+import sys
+from PyQt6.QtGui import QFont, QFontDatabase
+from PyQt6.QtWidgets import (
+    QApplication,
+    QLabel,
+    QLineEdit,
+    QMainWindow,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Simple VBox Layout")
+        self.setContentsMargins(12, 12, 12, 12)
+        self.resize(320, 240)
+
+        layout = QVBoxLayout()
+        title_label = QLabel("Simple VBox Layout!")
+
+        # add widgets & layouts to main layout
+        layout.addWidget(title_label)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(widget)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+
+app.exec()
