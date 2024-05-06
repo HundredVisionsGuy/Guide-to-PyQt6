4bfdd84d2a2e965d10069d02adb086048ff61698
Hundredvisionsguy <winikkc@hsd.k12.or.us>
2024-05-06 15:29:07 -0700

    did something?

--- guide_to_pyqt6/layouts/vbox_layout.py
+++ guide_to_pyqt6/layouts/vbox_layout.py
@@ -20,7 +20,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Simple VBox Layout")
+        self.setWindowTitle("A Semi-complete Guide to PyQt6!")
         self.setContentsMargins(12, 12, 12, 12)
         self.resize(320, 240)
 
@@ -28,9 +28,9 @@ class MainWindow(QMainWindow):
         layout = QVBoxLayout()
 
         # Title label
-        title_label = QLabel("Simple VBox Layout!")
+        title_label = QLabel("A Semi-complete Guide to PyQt6!")
         title_label.setFont(QFont("Calibri", 22, 800, True))
-        tagline_label = QLabel("Part 1: Getting started!")
+        tagline_label = QLabel("Intro: Project Setup!")
         tagline_label.setFont(QFont("Calibri", 18, 400, True))
 
         # Name input widget
@@ -40,7 +40,7 @@ class MainWindow(QMainWindow):
         # add widgets & layouts to main layout
         layout.addWidget(title_label)
         layout.addWidget(tagline_label)
-        layout.addWidget(name_input)
+        # layout.addWidget(name_input)
 
         # Push contents to the top
         layout.addStretch()
