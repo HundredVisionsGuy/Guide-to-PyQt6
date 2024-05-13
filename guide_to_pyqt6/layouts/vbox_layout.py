6b842e60a737ab30318a5637790bf7e50f1776cc
Hundredvisionsguy <winikkc@hsd.k12.or.us>
2024-05-13 10:10:41 -0700

    adding title screen for video

--- guide_to_pyqt6/layouts/vbox_layout.py
+++ guide_to_pyqt6/layouts/vbox_layout.py
@@ -20,15 +20,30 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Simple VBox Layout")
+        self.setWindowTitle("A Semi-complete Guide to PyQt6!")
         self.setContentsMargins(12, 12, 12, 12)
         self.resize(320, 240)
 
+        # Create main layout
         layout = QVBoxLayout()
-        title_label = QLabel("Simple VBox Layout!")
+
+        # Title label
+        title_label = QLabel("A Semi-complete Guide to PyQt6!")
+        title_label.setFont(QFont("Calibri", 22, 800, True))
+        tagline_label = QLabel("Intro: Project Setup!")
+        tagline_label.setFont(QFont("Calibri", 18, 400, True))
+
+        # Name input widget
+        name_input = QLineEdit()
+        name_input.setPlaceholderText("Your Name:")
 
         # add widgets & layouts to main layout
         layout.addWidget(title_label)
+        layout.addWidget(tagline_label)
+        # layout.addWidget(name_input)
+
+        # Push contents to the top
+        layout.addStretch()
 
         widget = QWidget()
         widget.setLayout(layout)
